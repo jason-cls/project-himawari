23d6d274f0c51b478c9ab77fa1e88e4bba052948
Jason Chong <jason.chongleeshin@gmail.com>
2020-06-09 17:55:19 -0400

    added template for user login

--- kaguya/users/routes.py
+++ kaguya/users/routes.py
@@ -1,3 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from kaguya.users.forms import LoginForm
 
 users = Blueprint('users', __name__)
+
+@users.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
