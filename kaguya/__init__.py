c3abf831f6688835e8f183b774c06e2a7ab47b01
Peter Sunga <44995752+peatear-anthony@users.noreply.github.com>
2020-06-10 12:42:47 +0900

    backend for db (register user)
    - able to register user (basic info)
    - need to hash passwords
    - add more user info to db table
    - moved config.py into kaguya package
    - function added to create new db

--- kaguya/__init__.py
+++ kaguya/__init__.py
@@ -1,22 +1,35 @@
 from flask import Flask
-from kaguya.admins.routes import admins
-from kaguya.anime.routes import anime
-from kaguya.main.routes import main
-from kaguya.users.routes import users
-from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from kaguya.config import Config
+
+# Extentions
+db = SQLAlchemy()
 
 
-def create_app():
+def create_app(create_db=False):
     # Creation of Flask-App
     app = Flask(__name__)
 
     # Set configuration
     app.config.from_object(Config)
 
-    # Register Blueprints
+
+
+    #Register Blueprint
+    # Need to put blueprint imports here to avoid circular import error
+    from kaguya.admins.routes import admins
+    from kaguya.anime.routes import anime
+    from kaguya.main.routes import main
+    from kaguya.users.routes import users
     app.register_blueprint(admins)
     app.register_blueprint(anime)
     app.register_blueprint(main)
     app.register_blueprint(users)
+
+    # DB stuff
+    db.init_app(app)
+    if create_db:
+        with app.app_context():
+            db.create_all()
 
     return app
--- kaguya/users/forms.py
+++ kaguya/users/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
 class LoginForm(FlaskForm):
@@ -9,4 +9,12 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
-# class RegistrationForm(FlaskForm):
+# Add hashing to password later 
+class RegisterForm(FlaskForm):
+    username = StringField('Username', 
+        validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', 
+        validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
