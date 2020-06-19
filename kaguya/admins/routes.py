6dcb827199710a769a48e9fb2ff2c89de938bcc7
Peter Sunga <44995752+peatear-anthony@users.noreply.github.com>
2020-06-19 18:17:31 +0900

    custom modelview added

--- kaguya/admins/routes.py
+++ kaguya/admins/routes.py
@@ -1,12 +1,11 @@
 from flask import Blueprint
-from flask_admin.contrib.sqla import ModelView
 from kaguya import db, admin
 from kaguya.models import User, Anime, UserAnime, Review
-from kaguya.admins.views import UserView, AnimeView
+from kaguya.admins.views import UserView, AnimeView, ReviewView, UserAnimeView
 
 admin_bp = Blueprint('admin_bp', __name__)
 
 admin.add_view(UserView(User, db.session))
 admin.add_view(AnimeView(Anime, db.session))
-admin.add_view(ModelView(UserAnime, db.session))
-admin.add_view(ModelView(Review, db.session))
+admin.add_view(UserAnimeView(UserAnime, db.session))
+admin.add_view(ReviewView(Review, db.session))
--- kaguya/admins/views.py
+++ kaguya/admins/views.py
@@ -1,13 +1,27 @@
 from flask_admin.contrib.sqla import ModelView
 
 class UserView(ModelView):
+    column_filters = ('id', 'username', 'email')
     column_searchable_list = ['id', 'username', 'email', 'image_file', 
         'datetime_created'] 
     column_list = ('id', 'username', 'email', 'image_file', 'datetime_created')
     page_size = 10
 
 class AnimeView(ModelView):
-    column_searchable_list = ['id', 'title'] 
+    column_searchable_list = ['title'] 
+    column_filters = ('id', 'title', 'status')
     column_list = ('id', 'title', 'episodes', 'rating', 
         'status','premiered','image_file', 'datetime_created')
+    page_size = 10
+
+class ReviewView(ModelView):
+    column_list = ('user_id', 'anime_id', 'content', 'rating', 
+        'datetime_created')
+    column_filters = ('user_id', 'anime_id')
+    page_size = 10
+
+class UserAnimeView(ModelView):
+    column_list = ('id', 'user_id','anime_id', 'status', 'episodes_watched',
+        'datetime_created')
+    column_filters = ('user_id', 'anime_id')
     page_size = 10
