43f1dfd86fd12d578fb0a029e8f4aac4dd68b643
Peter Sunga <44995752+peatear-anthony@users.noreply.github.com>
2020-06-19 23:45:35 +0900

    added staticfileview
    - Need to add "roles" to users so that only the admin can see admin page

--- kaguya/admins/views.py
+++ kaguya/admins/views.py
@@ -1,4 +1,9 @@
+import os.path as op
+from pathlib import Path
+from flask import redirect, url_for
+from flask_login import current_user
 from flask_admin.contrib.sqla import ModelView
+from flask_admin.contrib.fileadmin import FileAdmin
 
 class UserView(ModelView):
     column_filters = ('id', 'username', 'email')
@@ -7,6 +12,13 @@ class UserView(ModelView):
     column_list = ('id', 'username', 'email', 'image_file', 'datetime_created')
     page_size = 10
 
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self):
+        return redirect(url_for('users.login'))
+
+
 class AnimeView(ModelView):
     column_searchable_list = ['title'] 
     column_filters = ('id', 'title', 'status')
@@ -14,14 +26,41 @@ class AnimeView(ModelView):
         'status','premiered','image_file', 'datetime_created')
     page_size = 10
 
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self):
+        return redirect(url_for('users.login'))
+
+
 class ReviewView(ModelView):
     column_list = ('user_id', 'anime_id', 'content', 'rating', 
         'datetime_created')
     column_filters = ('user_id', 'anime_id')
     page_size = 10
 
+    def is_accessible(self):
+        return current_user.is_authenticated
+    def inaccessible_callback(self):
+        return redirect(url_for('users.login'))
+
+
 class UserAnimeView(ModelView):
     column_list = ('id', 'user_id','anime_id', 'status', 'episodes_watched',
         'datetime_created')
     column_filters = ('user_id', 'anime_id')
     page_size = 10
+
+    def is_accessible(self):
+        return current_user.is_authenticated
+    def inaccessible_callback(self):
+
+        return redirect(url_for('users.login'))
+
+
+class StaticFileView(FileAdmin):
+    def is_accessible(self):
+        return current_user.is_authenticated
+    def inaccessible_callback(self):
+
+        return redirect(url_for('users.login'))
