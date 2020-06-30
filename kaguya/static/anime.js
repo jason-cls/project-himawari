1546df150cb35bb9773e2703b2cb9a2457571d3b
Peter Sunga <44995752+peatear-anthony@users.noreply.github.com>
2020-07-01 00:23:04 +0900

    added: clickable rows to anime_list

--- kaguya/static/anime.js
+++ kaguya/static/anime.js
@@ -5,3 +5,18 @@ $('.dropdown-menu .status-btn').on('click', function(){
 $('.dropdown-menu .rate-btn').on('click', function(){
     $('.dropdown-toggle-rating').html($(this).html());
 })
+
+$(function(){
+    $('.table tr[data-href]').each(function(){
+        $(this).css('cursor','pointer').hover(
+            function(){ 
+                $(this).addClass('active'); 
+            },  
+            function(){ 
+                $(this).removeClass('active'); 
+            }).click( function(){ 
+                document.location = $(this).attr('data-href'); 
+            }
+        );
+    });
+});
