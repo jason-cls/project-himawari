cb2bba0e99703d7864221f70c844f7c43c7e1d52
Jason Chong <jason.chongleeshin@gmail.com>
2020-07-02 18:21:00 -0400

    added searchable dropdown buttons for filter options

--- kaguya/static/anime.js
+++ kaguya/static/anime.js
@@ -20,3 +20,13 @@ $(function(){
         );
     });
 });
+
+// Searchable dropdown
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".dropdown-menu a").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
