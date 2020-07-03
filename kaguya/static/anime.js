89f8a5fdf1a7817ac756e58fd192beef13a868e3
Jason Chong <jason.chongleeshin@gmail.com>
2020-07-02 20:11:22 -0400

    added more button formatting

--- kaguya/static/anime.js
+++ kaguya/static/anime.js
@@ -30,3 +30,12 @@ $(document).ready(function(){
     });
   });
 });
+
+$(document).ready(function(){
+  $("#yearInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".dropdown-menu a").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
