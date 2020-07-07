c9c70a286c7345ae7a7b35f0022cadbf8c03909e
Jason Chong <jason.chongleeshin@gmail.com>
2020-07-06 22:31:04 -0400

    functional filters and tags

--- kaguya/main/routes.py
+++ kaguya/main/routes.py
@@ -1,10 +1,9 @@
-from flask import Blueprint, render_template, current_app, request, url_for
+from flask import Blueprint, render_template, current_app, request, url_for, session
 from flask_login import login_required
 from kaguya.decorators import admin_required, permission_required
 from kaguya.models import Anime, Review, UserAnime
 from kaguya import db
 from sqlalchemy import func, desc
-from werkzeug.exceptions import BadRequestKeyError, HTTPException
 
 
 main = Blueprint('main', __name__)
@@ -26,6 +25,7 @@ def home():
     upcoming_anime = Anime.query.filter_by(premiered=upcoming_season).order_by(Anime.score.desc()) \
         .limit(n_anime).all()
     recent_reviews = Review.query.order_by(Review.datetime_created.desc()).limit(n_reviews).all()
+    session.clear()
     return render_template('home.html', title="Home Page",
                            seasonal_anime=seasonal_anime, popular_anime=popular_anime,
                            upcoming_anime=upcoming_anime, recent_reviews=recent_reviews)
@@ -44,16 +44,19 @@ def reviews():
     q_reviews = Review.query.order_by(Review.datetime_created.desc())
 
     # Filter queries
-    if len(request.form) != 0:
+    if request.method == 'POST':
+        session['filterForm'] = request.form.to_dict(flat=False)
+
+    if session.get('filterForm'):
         q_reviews = Review.query
-        ratingFilter = request.form.get('ratingRadios')
+        ratingFilter = session['filterForm'].get('ratingRadios')[0]
 
         if ratingFilter == 'lowest rating':
             q_reviews = q_reviews.order_by(Review.rating)
         elif ratingFilter == 'highest rating':
             q_reviews = q_reviews.order_by(Review.rating.desc())
 
-        timeFilter = request.form.get('timeRadios')
+        timeFilter = session['filterForm'].get('timeRadios')[0]
         if timeFilter == 'oldest':
             q_reviews = q_reviews.order_by(Review.datetime_created)
         elif timeFilter == 'recent':
@@ -69,12 +72,18 @@ def reviews():
         next_url = url_for('main.reviews', page=q_reviews.next_num)
     else:
         next_url = None
-    return render_template('reviews.html', title='User Reviews', reviews=q_reviews.items,
-                           prev_url=prev_url, next_url=next_url)
+    first_url = url_for('main.reviews', page=1)
+    last_url = url_for('main.reviews', page=q_reviews.pages)
+
+    activeFilters = session.get('filterForm')
+    if not activeFilters:
+        activeFilters = {}
+
+    return render_template('reviews.html', title='User Reviews', reviews=q_reviews.items, activeFilters=activeFilters,
+                           prev_url=prev_url, next_url=next_url, first_url=first_url, last_url=last_url)
 
 
 @main.route('/explore', methods=['GET', 'POST'])
-@admin_required  # Just example for how to use this admin decorator
 def explore():
     n_anime_per_page = 12
     page = request.args.get('page', 1, type=int)
@@ -102,12 +111,62 @@ def explore():
                 tags['year'].append(year)
     tags['year'].sort()
 
-    # Filters / active tags
-    activeTags = {'genres': [],
-                  'year': [],
-                  'season': [],
-                  'status': [],
-                  'type': []}
+
+    # Filter queries
+    if request.method == 'POST':
+        session['filterForm'] = request.form.to_dict(flat=False)
+
+    if session.get('filterForm'):
+        # Tags
+        q_anime = Anime.query
+        if session['filterForm'].get('genreCheckboxes'):
+            for genre in session['filterForm'].get('genreCheckboxes'):
+                q_anime = q_anime.filter(Anime.genres.contains(genre))
+        year_subqueries = []
+        if session['filterForm'].get('yearCheckboxes'):
+            for year in session['filterForm'].get('yearCheckboxes'):
+                q = q_anime.filter(Anime.premiered.contains(year))
+                year_subqueries.append(q)
+            q_anime = year_subqueries[0]
+            for q in year_subqueries[1:]:
+                q_anime = q_anime.union(q)
+        season_subqueries = []
+        if session['filterForm'].get('seasonCheckboxes'):
+            for season in session['filterForm'].get('seasonCheckboxes'):
+                q = q_anime.filter(Anime.premiered.contains(season))
+                season_subqueries.append(q)
+            q_anime = season_subqueries[0]
+            for q in season_subqueries[1:]:
+                q_anime = q_anime.union(q)
+        status_subqueries = []
+        if session['filterForm'].get('statusCheckboxes'):
+            for status in session['filterForm'].get('statusCheckboxes'):
+                q = q_anime.filter(Anime.status.contains(status))
+                status_subqueries.append(q)
+            q_anime = status_subqueries[0]
+            for q in status_subqueries[1:]:
+                q_anime = q_anime.union(q)
+        type_subqueries = []
+        if session['filterForm'].get('typeCheckboxes'):
+            for formatType in session['filterForm'].get('typeCheckboxes'):
+                q = q_anime.filter(Anime.type.contains(formatType))
+                type_subqueries.append(q)
+            q_anime = type_subqueries[0]
+            for q in type_subqueries[1:]:
+                q_anime = q_anime.union(q)
+
+        # Ordering
+        ratingFilter = session['filterForm'].get('ratingRadios')[0]
+        if ratingFilter == 'lowest rating':
+            q_anime = q_anime.order_by(Anime.score)
+        elif ratingFilter == 'highest rating':
+            q_anime = q_anime.order_by(Anime.score.desc())
+
+        timeFilter = session['filterForm'].get('timeRadios')[0]
+        if timeFilter == 'oldest':
+            q_anime = q_anime.order_by(Anime.datetime_created)
+        elif timeFilter == 'recent':
+            q_anime = q_anime.order_by(Anime.datetime_created.desc())
 
     # Pagination
     q_anime = q_anime.paginate(page, n_anime_per_page, True)
@@ -121,6 +180,11 @@ def explore():
         next_url = None
     first_url = url_for('main.explore', page=1)
     last_url = url_for('main.explore', page=q_anime.pages)
+
+    activeFilters = session.get('filterForm')
+    if not activeFilters:
+        activeFilters = {}
+
     return render_template('explore.html', title="Browse Anime", animes=q_anime.items,
-                           tags=tags.items(), activeTags=activeTags.items(),
+                           tags=tags.items(), activeFilters=activeFilters,
                            first_url=first_url, last_url=last_url, prev_url=prev_url, next_url=next_url)
