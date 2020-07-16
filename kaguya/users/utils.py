2fad0a6ceb6647fd4a524f8313f057dfdf08853d
Jason Chong <jason.chongleeshin@gmail.com>
2020-07-15 22:45:33 -0400

    removed secrets lib

--- kaguya/users/utils.py
+++ kaguya/users/utils.py
@@ -1,14 +1,13 @@
 import os
-import secrets
 from flask import current_app
 from PIL import Image
-from flask import url_for
+from random import randint
 
 
 def save_picture(form_picture):
     # Change uploaded picture to random name keep ext.
     # Also resizes the photo
-    random_hex = secrets.token_hex(8)
+    random_hex = randint(0, 9999999)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
