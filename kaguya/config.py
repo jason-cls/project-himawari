7658b958a88cc9a53268e3fff0b977493f1dd7f7
Jason Chong <jason.chongleeshin@gmail.com>
2020-07-15 22:28:03 -0400

    deployment

--- kaguya/config.py
+++ kaguya/config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    DEBUG_MODE = True
+    DEBUG_MODE = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'  # For security
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')
     FLASK_ADMIN_SWATCH = 'darkly' # theme for admin (https://bootswatch.com/3/)
